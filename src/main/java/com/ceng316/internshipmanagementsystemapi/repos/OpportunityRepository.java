99736466f37d72c3a3b99e91c5f6c18649bb6df6
esmairemtek <esmairemtek12@gmail.com>
2024-03-26 16:25:51 +0300

    error 63 fixed

--- src/main/java/com/ceng316/internshipmanagementsystemapi/repos/OpportunityRepository.java
+++ src/main/java/com/ceng316/internshipmanagementsystemapi/repos/OpportunityRepository.java
@@ -1,11 +1,13 @@
 package com.ceng316.internshipmanagementsystemapi.repos;
 
 import com.ceng316.internshipmanagementsystemapi.entities.Document;
+import com.ceng316.internshipmanagementsystemapi.entities.Opportunity;
 
 import java.util.List;
 
 public interface OpportunityRepository extends JpaRepository<Document, Long> {
 
+    Opportunity findOpportunity(Long id);
     List<Document> findAllOpportunities();
     List<Document> findAllByStatusAndType();
 }
