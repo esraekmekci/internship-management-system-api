ab9c547e10109e546726c7a52953e92e9a51edbc
KaganAsl <kagan.aslancan@gmail.com>
2024-05-13 14:48:44 +0300

    Secretary request for getting student list

--- src/main/java/com/ceng316/internshipmanagementsystemapi/controllers/SecretaryController.java
+++ src/main/java/com/ceng316/internshipmanagementsystemapi/controllers/SecretaryController.java
@@ -1,11 +1,27 @@
 package com.ceng316.internshipmanagementsystemapi.controllers;
 
-import com.ceng316.internshipmanagementsystemapi.services.SecretaryService;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ceng316.internshipmanagementsystemapi.entities.Student;
+import com.ceng316.internshipmanagementsystemapi.services.SecretaryService;
+import com.ceng316.internshipmanagementsystemapi.services.StudentService;
+
+@RestController
+@RequestMapping("/secretary")
 public class SecretaryController {
     SecretaryService secretaryService;
 
-    void downloadStudentList() {
+    public SecretaryController(SecretaryService secretaryService) {
+        this.secretaryService = secretaryService;
+    }
+
+    @GetMapping("/studentList")
+    public List<Student> getAllStudents() {
+        return secretaryService.getStudentList();
     }
 
     void uploadSgkCertificate() {
