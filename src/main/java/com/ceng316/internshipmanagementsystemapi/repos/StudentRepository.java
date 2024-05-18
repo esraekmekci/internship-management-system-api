122727a94dd8d50ec0d0391b6710587d2a636f78
esmairemtek <esmairemtek12@gmail.com>
2024-05-18 23:13:53 +0300

    added nationality to student

--- src/main/java/com/ceng316/internshipmanagementsystemapi/repos/StudentRepository.java
+++ src/main/java/com/ceng316/internshipmanagementsystemapi/repos/StudentRepository.java
@@ -3,8 +3,12 @@ package com.ceng316.internshipmanagementsystemapi.repos;
 import com.ceng316.internshipmanagementsystemapi.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findByEmail(String email);
+
+    List<Student> findByNationality(String nationality);
 
 //    SGKCertificate findSGKCertificate(Long id);
 
