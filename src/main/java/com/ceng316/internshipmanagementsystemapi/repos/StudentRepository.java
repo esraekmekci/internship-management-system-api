7e1870da1c9f481289d8872237a7fdeec4ea6292
KaganAsl <kagan.aslancan@gmail.com>
2024-05-21 23:03:41 +0300

    Comments changed

--- src/main/java/com/ceng316/internshipmanagementsystemapi/repos/StudentRepository.java
+++ src/main/java/com/ceng316/internshipmanagementsystemapi/repos/StudentRepository.java
@@ -9,14 +9,14 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findByEmail(String email);
 
-//    @Query("SELECT s " +
-//            "FROM Student s " +
-//            "JOIN Application a ON s.studentID = a.student.studentID " +
-//            "JOIN CompanyRep cr ON a.company.companyid = cr.companyid " +
-//            "WHERE s.nationality = :nationality " +
-//            "AND s.internshipStatus = :internshipStatus " +
-//            "AND cr.companyAddress LIKE %:companyAddress ")
-//    List<Student> findByNationalityAndInternshipStatusAndCompanyAddress(String nationality, String internshipStatus, String companyAddress);
+    @Query("SELECT s " +
+            "FROM Student s " +
+            "JOIN Application a ON s.studentID = a.student.studentID " +
+            "JOIN CompanyRep cr ON a.company.companyid = cr.companyid " +
+            "WHERE s.nationality = :nationality " +
+            "AND s.internshipStatus = :internshipStatus " +
+            "AND cr.companyAddress LIKE %:companyAddress ")
+    List<Student> findByNationalityAndInternshipStatusAndCompanyAddress(String nationality, String internshipStatus, String companyAddress);
 //    SGKCertificate findSGKCertificate(Long id);
 
 }
