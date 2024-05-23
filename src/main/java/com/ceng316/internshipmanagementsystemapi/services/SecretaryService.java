49d62bc72417484c6674ffd0e2381ac5499ac9cd
KaganAsl <kagan.aslancan@gmail.com>
2024-05-23 11:40:07 +0300

    Changes for secretary

--- src/main/java/com/ceng316/internshipmanagementsystemapi/services/SecretaryService.java
+++ src/main/java/com/ceng316/internshipmanagementsystemapi/services/SecretaryService.java
@@ -7,7 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ceng316.internshipmanagementsystemapi.controllers.S3Controller;
+import com.ceng316.internshipmanagementsystemapi.entities.Application;
 import com.ceng316.internshipmanagementsystemapi.entities.SGKFile;
+import com.ceng316.internshipmanagementsystemapi.repos.ApplicationRepository;
 import com.ceng316.internshipmanagementsystemapi.repos.SGKRepository;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
@@ -29,19 +31,28 @@ public class SecretaryService {
     StudentRepository studentRepo;
     SGKRepository sgkRepo;
     S3Controller s3Controller;
+    ApplicationRepository applicationRepo;
 
-    public SecretaryService(SecretaryRepository secretaryRepo, StudentRepository studentRepo, SGKRepository sgkRepo, S3Controller s3Controller) {
+    public SecretaryService(SecretaryRepository secretaryRepo, StudentRepository studentRepo, SGKRepository sgkRepo, S3Controller s3Controller, ApplicationRepository applicationRepo) {
         this.secretaryRepo = secretaryRepo;
         this.studentRepo = studentRepo;
         this.sgkRepo = sgkRepo;
         this.s3Controller = s3Controller;
+        this.applicationRepo = applicationRepo;
     }
     public List<Student> getEligibleStudentsList() {
-        return studentRepo.findByNationalityAndInternshipStatusAndCompanyAddress("Turkish", "Application Form Approved", "TR");
+        List<Student> sL1 = studentRepo.findByNationalityAndInternshipStatusAndCompanyAddress("Turkish", "Application Form Approved", "TR");
+        List<Student> sL2 = studentRepo.findByNationalityAndInternshipStatusAndCompanyAddress("Turkish", "SGK Document Pending", "TR");
+        List<Student> sL3 = studentRepo.findByNationalityAndInternshipStatusAndCompanyAddress("Turkish", "SGK Document Uploaded", "TR");
+        List<Student> students = new ArrayList<>();
+        students.addAll(sL1);
+        students.addAll(sL2);
+        students.addAll(sL3);
+        return students;
     }
 
     public List<StudentSGKResponse> getEligibleStudentsWithStatus() {
-        List<Student> studentList = studentRepo.findByNationalityAndInternshipStatusAndCompanyAddress("Turkish", "Application Form Approved", "TR");
+        List<Student> studentList = getEligibleStudentsList();
         List<StudentSGKResponse> studentSGKResponseList = new ArrayList<>();
         for (Student s : studentList) {
             SGKFile file = sgkRepo.findByStudentId(s.getStudentID());
@@ -77,7 +88,14 @@ public class SecretaryService {
         try {
             String fileName = "SGK_Report_" + studentId;
             s3Controller.uploadFile(file, fileName);
-            sgkRepo.updateSGKDocumentStatus(studentId, "Available");
+            sgkRepo.updateSGKDocumentStatus(studentId, "SGK Document Uploaded");
+            Application application = applicationRepo.findByStudentIdAndApplicationStatus(studentId, "Application Form Approved");
+            if (application == null) {
+                application = applicationRepo.findByStudentIdAndApplicationStatus(studentId, "SGK Document Pending");
+            }
+            if (application != null) {
+                application.setApplicationStatus("SGK Document Uploaded");
+            }
         } catch (Exception e) {
             throw new Exception("Error: " + e.getMessage());
         }
@@ -87,7 +105,11 @@ public class SecretaryService {
         try {
             String fileName = "SGK_Report_" + studentId;
             s3Controller.deleteFile(fileName);
-            sgkRepo.updateSGKDocumentStatus(studentId, "Unavailable");
+            sgkRepo.updateSGKDocumentStatus(studentId, "SGK Document Pending");
+            Application application = applicationRepo.findByStudentIdAndApplicationStatus(studentId, "SGK Document Uploaded");
+            if (application != null) {
+                application.setApplicationStatus("SGK Document Pending");
+            }
         } catch (Exception e) {
             throw new Exception("Error: " + e.getMessage());
         }
